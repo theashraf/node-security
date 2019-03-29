92b6aeae066afb8a52fa8053b2599d5a9a15b228
Abdelrahman Ashraf <a.theashraf@gmail.com>
2019-03-29 16:20:35 +0200

    refactor: Maintain Package Dependencies

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import routes from './src/routes/crmRoutes';
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import routes from "./src/routes/crmRoutes";
 
 const app = express();
 const PORT = 3000;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRMdb', {
-    useMongoClient: true
-});
+mongoose.connect("mongodb://localhost/CRMdb", { useNewUrlParser: true });
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,12 +17,10 @@ app.use(bodyParser.json());
 routes(app);
 
 // serving static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
+app.get("/", (req, res) =>
+  res.send(`Node and express server is running on port ${PORT}`)
 );
 
-app.listen(PORT, () =>
-    console.log(`your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`your server is running on port ${PORT}`));
