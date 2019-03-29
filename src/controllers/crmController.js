bc4c80fb56fecada5c982014b8f23d608991c1e8
Abdelrahman Ashraf <a.theashraf@gmail.com>
2019-03-29 21:44:59 +0200

    fix: data handling with type and validation

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -1,10 +1,35 @@
 import mongoose from 'mongoose';
+import Joi from "joi"
 import { ContactSchema } from '../models/crmModel';
 
 const Contact = mongoose.model('Contact', ContactSchema);
 
+const addNewContactSchema = Joi.object.keys({
+    firstName: Joi.string().min(2).max(50).alphanum().trim().required(),
+    lastName: Joi.string().min(2).max(50).alphanum().trim().required(),
+    email: Joi.string().email(),
+    company: Joi.string().min(3).max(255).alphanum().trim(),
+    phone: Joi.number(),
+    created_at: Joi.date()
+})
+
+const updateContactSchema = Joi.object.keys({
+    firstName: Joi.string().min(2).max(50).alphanum().trim(),
+    lastName: Joi.string().min(2).max(50).alphanum().trim(),
+    email: Joi.string().email(),
+    company: Joi.string().min(3).max(255).alphanum().trim(),
+    phone: Joi.number(),
+    created_at: Joi.date()
+})
+
 export const addNewContact = (req, res) => {
-    let newContact = new Contact(req.body);
+    const { error , value : contactData } = Joi.validate(req.body,addNewContactSchema);
+
+    if(error) {
+        return res.send(error.details[0].message)
+    }
+
+    let newContact = new Contact(contactData);
 
     newContact.save((err, contact) => {
         if (err) {
@@ -33,7 +58,13 @@ export const getContactWithID = (req, res) => {
 }
 
 export const updateContact = (req, res) => {
-    Contact.findOneAndUpdate({ _id: req.params.contactId}, req.body, { new: true }, (err, contact) => {
+    const { error, value : contactData } = Joi.validate(req.body,updateContactSchema);
+
+    if(error) {
+        return res.send(error.details[0].message)
+    }
+    
+    Contact.findOneAndUpdate({ _id: req.params.contactId}, contactData, { new: true }, (err, contact) => {
         if (err) {
             res.send(err);
         }
